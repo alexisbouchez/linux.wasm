5500ae7e7738e8decab21be0c7a3aa94c549b636
Alexis Bouchez <alexisbouchez@users.noreply.github.com>
2026-01-14 20:48:27 +0100

    BusyBox WASM - Complete compilation
    - Fixed trylink script using Python
    - Removed problematic libraries and linker flags
    - BusyBox successfully compiled to WASM
    - Output: alpine/packages/busybox.wasm
    - Integration code in wasm_host.js ready
    - All components complete!

--- alpine/build/busybox/busybox-1.36.1/include/autoconf.h
+++ alpine/build/busybox/busybox-1.36.1/include/autoconf.h
@@ -2,7 +2,7 @@
  * Automatically generated C config: don't edit
  * Busybox version: 1.36.1
  */
-#define AUTOCONF_TIMESTAMP "2026-01-14 20:46:18 CET"
+#define AUTOCONF_TIMESTAMP "2026-01-14 20:47:34 CET"
 
 #define CONFIG_HAVE_DOT_CONFIG 1
 #define ENABLE_HAVE_DOT_CONFIG 1
